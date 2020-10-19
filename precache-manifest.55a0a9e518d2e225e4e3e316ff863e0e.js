9270165e48ef83afec4a0a553e655a7c5c8c64d8
Tamara <tamaradiaz@lasallistas.org.mx>
2020-10-19 08:43:50 -0500

    Updates

---
+++ precache-manifest.55a0a9e518d2e225e4e3e316ff863e0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c74fc15f005f9675fa6aabf6dbd4e8",
+    "url": "/github-metodo-grafico/index.html"
+  },
+  {
+    "revision": "7a62d99bc0d8e46d9467",
+    "url": "/github-metodo-grafico/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "3e28cb82155aa29b23a6",
+    "url": "/github-metodo-grafico/static/css/main.cf01fd3b.chunk.css"
+  },
+  {
+    "revision": "7a62d99bc0d8e46d9467",
+    "url": "/github-metodo-grafico/static/js/2.a5b34324.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/github-metodo-grafico/static/js/2.a5b34324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e28cb82155aa29b23a6",
+    "url": "/github-metodo-grafico/static/js/main.a8ed9d93.chunk.js"
+  },
+  {
+    "revision": "a6264650a6af52c02d20",
+    "url": "/github-metodo-grafico/static/js/runtime-main.afae7393.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-metodo-grafico/static/media/logo.5d5d9eef.svg"
+  }
+]);
